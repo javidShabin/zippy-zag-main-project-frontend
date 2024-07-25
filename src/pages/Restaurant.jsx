df32a5b0d4d0c23ff4949b50046aa3c6913a092f
javid shabin <javid.main.act@gmail.com>
2024-07-26 00:23:44 +0530

    transfor the code to previous

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -28,34 +28,33 @@ const Restaurant = () => {
         Our <span className="text-[#eb97f1]">Restaurants</span>
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {restData.map((restaurant) => (
-          <div
-            onClick={() => {
-              navigate(`/user/rest-details/${restaurant._id}`);
-            }}
-            className="relative w-full h-[250px] rounded-lg shadow-lg overflow-hidden bg-gray-200"
-            style={{
-              backgroundImage: `url(${restaurant.image})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-            key={restaurant._id}
-          >
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-[#0000004f] bg-opacity-40"></div>
+        {restData.length > 0 ? (
+          restData.map((restaurant) => (
+            <div
+              onClick={() => {
+                navigate(`/user/rest-details/${restaurant._id}`);
+              }}
+              className="relative w-full h-[250px] rounded-lg shadow-lg overflow-hidden bg-gray-200"
+              style={{
+                backgroundImage: `url(${restaurant.image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              key={restaurant._id}
+            >
+              {/* Overlay */}
+              <div className="absolute inset-0 bg-[#0000004f] bg-opacity-40"></div>
 
-            {/* Content */}
-            <div className="relative z-10 p-4 flex flex-col justify-end h-full text-white">
-              <h2 className="text-lg font-semibold">{restaurant.name}</h2>
-              <p className="text-sm">{restaurant.location}</p>
-              <p className="text-xs mt-2">{restaurant.description}</p>
+              {/* Content */}
+              <div className="relative z-10 p-4 flex flex-col justify-end h-full text-white">
+                <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+                <p className="text-sm">{restaurant.location}</p>
+                <p className="text-xs mt-2">{restaurant.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
-        {restData.length === 0 && (
-          <div className="flex justify-center items-center w-full h-full">
-            <span className="loading loading-dots loading-md bg-[#cd50f0]"></span>
-          </div>
+          ))
+        ) : (
+          <div className="flex justify-center items-center "><span className="loading loading-dots loading-md bg-[#cd50f0]"></span></div>
         )}
       </div>
     </main>
