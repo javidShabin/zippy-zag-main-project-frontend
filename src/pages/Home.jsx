509e86461cce027d7d6ff0b6350e0bbb9e368976
javid shabin <javid.main.act@gmail.com>
2024-08-30 16:02:29 +0530

    Add the animation for images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,23 +12,23 @@ const Home = () => {
       >
         <img
           src={mintImage}
-          className="w-[50px] sm:w-[80px] absolute top-10 sm:top-20 right-16 sm:right-56 animate-pulse transition duration-700"
+          className="w-[50px] sm:w-[80px] absolute top-10 sm:top-20 right-16 sm:right-56 animate-slideRight"
         />
         <img
           src={tomatoImage}
-          className="w-[50px] sm:w-[80px] absolute bottom-5 right-20 sm:right-80 animate-pulse transition duration-700"
+          className="w-[50px] sm:w-[80px] absolute bottom-5 right-20 sm:right-80 animate-slideRight"
         />
         <img
           src={mint1Image}
-          className="w-[70px] sm:w-[120px] absolute bottom-5 left-20 sm:left-80 animate-pulse transition duration-700"
+          className="w-[70px] sm:w-[120px] absolute bottom-5 left-20 sm:left-80 animate-slideRight"
         />
         <img
           src={tomato1Image}
-          className="w-[90px] sm:w-[150px] absolute top-5 sm:top-10 left-14 sm:left-56 animate-pulse transition duration-700"
+          className="w-[90px] sm:w-[150px] absolute top-5 sm:top-10 left-14 sm:left-56 animate-slideRight"
         />
         <img
           src={chillyImage}
-          className="w-[70px] sm:w-[110px] absolute bottom-20 sm:bottom-60 right-0 animate-pulse transition duration-700"
+          className="w-[70px] sm:w-[110px] absolute bottom-20 sm:bottom-60 right-0 animate-slideRight"
         />
         <img
           src={chilly1Image}
