bfc8a3db31eaf8159c16a236fa41d0367ff69e5b
javid shabin <javid.main.act@gmail.com>
2024-11-17 23:50:11 +0530

    install and configh the react router dome for routing the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/routes";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
