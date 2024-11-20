f660dffb52ceeaaee7ff6ae762c03b2b91a6725d
javid shabin <javid.main.act@gmail.com>
2024-11-20 22:46:37 +0530

    Create the header and create layout file for user and call the header in the layout file and add the lay out in the router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header>
+      <div className="container">
+        <div className="logo">
+          <h1>ZippyZag</h1>
+        </div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Restaurants</li>
+            <li>Contact</li>
+          </ul>
+          <div>
+            <span>Join us</span>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import UserLayout from "../layout/UserLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <UserLayout/>,
   },
 ]);
