3efe950c49eba7e1e37281013fe1b129d6949bff
javid shabin <javid.main.act@gmail.com>
2024-11-23 12:54:48 +0530

    add the condition for logined user header changein

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import UserHeader from "../components/loginedUser/Header";
+import { useDispatch, useSelector } from "react-redux";
+import { clearUser, saveUser } from "../redux/features/userSlice";
 
 const UserLayout = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const [loading, setLoading] = useState(true); // Start as true and set to false after checking
+  const { isUserExist } = useSelector((state) => state.user);
+
+  const checkUser = async () => {
+    try {
+      await axiosInstants({
+        method: "GET",
+        url: "/user/check-user",
+      });
+      dispatch(saveUser());
+    } catch (error) {
+      dispatch(clearUser());
+      console.error("Error checking user:", error);
+    } finally {
+      setLoading(false); // Set loading to false after checking user
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, [location.pathname]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-spinner text-warning"></span>
+      </div>
+    ); // Optionally show a loading indicator
+  }
+
   return (
     <div>
-      <Header />
-      <Outlet/>
+      {isUserExist ? <UserHeader/> : <Header />}
+      <Outlet />
     </div>
   );
 };
