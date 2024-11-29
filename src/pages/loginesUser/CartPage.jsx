a7f0bc323a93ecdc77518db69df8c70542c3ebcb
javid shabin <javid.main.act@gmail.com>
2024-11-30 00:44:15 +0530

    add the new images for the new design change hero section background and the main image and add some leav image for beauti of the hero section

--- src/pages/loginesUser/CartPage.jsx
+++ src/pages/loginesUser/CartPage.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { axiosInstance } from "../../config/axiosInstance";
 
 const CartPage = () => {
+  const getTheCart = async () => {
+    try {
+      const response = await axiosInstance({
+        method: "GET",
+        url: "/cart/getCart",
+      });
+      console.log(response);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    getTheCart();
+  }, []);
   return (
     <section>
-      <div className="conatiner">
-        
-      </div>
+      <div className="conatiner"></div>
     </section>
-  )
-}
+  );
+};
 
-export default CartPage
+export default CartPage;
