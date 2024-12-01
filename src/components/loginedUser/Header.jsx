06654ebde6f9dcc3ab3e75823d6676b774ff4278
javid shabin <javid.main.act@gmail.com>
2024-12-01 12:03:56 +0530

    navlink style chamhe in logined user header also

--- src/components/loginedUser/Header.jsx
+++ src/components/loginedUser/Header.jsx
@@ -49,7 +49,7 @@ const UserHeader = () => {
             <Link to={"/"}>
               <li
                 onClick={toggleMenu}
-                className="text-white block font-semibold py-1 px-3 relative overflow-hidden group"
+                className="block font-semibold py-1 px-3 bg-[#ffa100] text-white rounded-full shadow-lg hover:shadow-[#ffa100] transition duration-300 relative overflow-hidden group"
               >
                 Home
               </li>
@@ -57,7 +57,7 @@ const UserHeader = () => {
             <Link to={"/about"}>
               <li
                 onClick={toggleMenu}
-                className="text-white block font-semibold py-1 px-3 relative overflow-hidden group"
+                className="block font-semibold py-1 px-3 bg-[#ffa100] text-white rounded-full shadow-lg hover:shadow-[#ffa100] transition duration-300 relative overflow-hidden group"
               >
                 About
               </li>
@@ -65,7 +65,7 @@ const UserHeader = () => {
             <Link to={"/restaurant"}>
               <li
                 onClick={toggleMenu}
-                className="text-white block font-semibold py-1 px-3 relative overflow-hidden group"
+                className="block font-semibold py-1 px-3 bg-[#ffa100] text-white rounded-full shadow-lg hover:shadow-[#ffa100] transition duration-300 relative overflow-hidden group"
               >
                 Restaurants
               </li>
