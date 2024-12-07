f599ffd8873a0c0a7e4d933704a32cd76ff312fe
javid shabin <javid.main.act@gmail.com>
2024-12-08 01:18:31 +0530

    create the component for add the filtering data

---
+++ src/components/filterData/FilterData.jsx
@@ -0,0 +1,55 @@
+import {
+  biriyani,
+  burger,
+  iceCream,
+  juice,
+  mojito,
+  sandwich,
+} from "../../assets";
+
+const filterData = [
+  {
+    id: 1,
+    name: "Biriyani",
+    image: biriyani,
+    category: "Biriyani",
+  },
+  {
+    id: 2,
+    name: "Burger",
+    image: burger,
+    category: "Burger",
+  },
+  {
+    id: 3,
+    name: "Sandwich",
+    image: sandwich,
+    category: "Sandwich",
+  },
+  {
+    id: 4,
+    name: "Juice",
+    image: juice,
+    category: "Juice",
+  },
+  {
+    id: 5,
+    name: "Pasta",
+    image: pasta,
+    category: "Pasta",
+  },
+  {
+    id: 6,
+    name: "Mojito",
+    image: mojito,
+    category: "Mojito",
+  },
+  {
+    id: 7,
+    name: "Ice cream",
+    image: iceCream,
+    category: "Ice creami",
+  },
+];
+
+export { filterData };
