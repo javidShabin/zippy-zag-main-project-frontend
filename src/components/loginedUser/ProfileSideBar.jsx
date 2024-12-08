ec5fac6bcc6c1c6de8e2736d77023aae99b18908
javid shabin <javid.main.act@gmail.com>
2024-12-09 01:31:14 +0530

    make logout function on the button

--- src/components/loginedUser/ProfileSideBar.jsx
+++ src/components/loginedUser/ProfileSideBar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { axiosInstance } from "../../config/axiosInstance";
 
 const ProfileSideBar = () => {
+  const handleLogout = async () => {
+    try {
+      const response = await axiosInstance({
+        method: "POST",
+        url: "/user/logout",
+      });
+      console.log(response);
+    } catch (error) {}
+  };
   return (
     <div className="w-64 bg-gray-100 h-screen p-4">
       <h2 className="text-2xl font-bold text-gray-800 mb-6">
@@ -41,7 +51,10 @@ const ProfileSideBar = () => {
             Address
           </Link>
         </li>
-        <li className="block px-4 py-2 rounded-lg text-gray-700 bg-orange-400 hover:text-white transition">
+        <li
+          onClick={handleLogout}
+          className="block px-4 py-2 rounded-lg text-gray-700 bg-orange-400 hover:text-white transition"
+        >
           Log Out
         </li>
       </ul>
