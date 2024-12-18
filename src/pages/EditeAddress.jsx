cbf4a6c7dedf039fba304918109b1ab0f1c758db
javid shabin <javid.main.act@gmail.com>
2024-12-19 01:45:01 +0530

    solve the errors

--- src/pages/EditeAddress.jsx
+++ src/pages/EditeAddress.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, {  useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { axiosInstance } from "../../config/axiosInstance";
 
 const EditeAddress = () => {
   
